e6aea9410000baad515a402a51c1376f66e78221
jinroq <jinroq@gmail.com>
2026-01-31 19:35:58 +0900

    refactor: Omitted DateTime tests.

--- test/ruby_date/test_date_compat.rb
+++ test/ruby_date/test_date_compat.rb
@@ -6,18 +6,20 @@ require_relative '../../lib/ruby_date'
 class RubyDateDateCompatTest < Test::Unit::TestCase
 
   def test_compat
-    assert_equal(RubyDate.new, RubyDate.new)
-    assert_equal(RubyDate.new(2002,3,19), RubyDate.new(2002,3,19))
-    assert_equal(RubyDate.new(2002,3,19, 0,0,0), RubyDate.new(2002,3,19))
-    assert_equal(RubyDate.new(2002,3,19, 0,0,0, 0), RubyDate.new(2002,3,19))
-    assert_equal(RubyDate.new(2002,3,19, 0,0,0, 0.to_r), RubyDate.new(2002,3,19))
-    assert_equal(RubyDate.new(2002,3,19, 0,0,0, 0, RubyDate::GREGORIAN), RubyDate.new(2002,3,19, RubyDate::GREGORIAN))
-    assert_equal(RubyDate.new(2002,3,19, 0,0,0, 0, RubyDate::JULIAN), RubyDate.new(2002,3,19, RubyDate::JULIAN))
+    omit "DateTime is deprecated and not implemented in RubyDate"
 
-    assert(RubyDate.new(2002,3,19) != RubyDate.new(2002,3,19, 12,0,0))
-    assert(RubyDate.new(2002,3,19) != RubyDate.new(2002,3,19, 0,0,1))
-    assert(RubyDate.new(2002,3,19) === RubyDate.new(2002,3,19, 12,0,0))
-    assert(RubyDate.new(2002,3,19) === RubyDate.new(2002,3,19, 0,0,1))
+    assert_equal(DateTime.new, RubyDate.new)
+    assert_equal(DateTime.new(2002,3,19), RubyDate.new(2002,3,19))
+    assert_equal(DateTime.new(2002,3,19, 0,0,0), RubyDate.new(2002,3,19))
+    assert_equal(DateTime.new(2002,3,19, 0,0,0, 0), RubyDate.new(2002,3,19))
+    assert_equal(DateTime.new(2002,3,19, 0,0,0, 0.to_r), RubyDate.new(2002,3,19))
+    assert_equal(DateTime.new(2002,3,19, 0,0,0, 0, RubyDate::GREGORIAN), RubyDate.new(2002,3,19, RubyDate::GREGORIAN))
+    assert_equal(DateTime.new(2002,3,19, 0,0,0, 0, RubyDate::JULIAN), RubyDate.new(2002,3,19, RubyDate::JULIAN))
+
+    assert(RubyDate.new(2002,3,19) != DateTime.new(2002,3,19, 12,0,0))
+    assert(RubyDate.new(2002,3,19) != DateTime.new(2002,3,19, 0,0,1))
+    assert(RubyDate.new(2002,3,19) === DateTime.new(2002,3,19, 12,0,0))
+    assert(RubyDate.new(2002,3,19) === DateTime.new(2002,3,19, 0,0,1))
   end
 
 end
