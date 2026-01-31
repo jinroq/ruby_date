ddd4f4a264b531f87ef9e709ea42693a97f1d39e
jinroq <jinroq@gmail.com>
2026-01-31 19:19:40 +0900

    refactor: Comment out DateTime tests

--- test/ruby_date/test_date_attr.rb
+++ test/ruby_date/test_date_attr.rb
@@ -7,65 +7,103 @@ class RubyDateDateAttrTest < Test::Unit::TestCase
 
   def test__attr
     date = RubyDate.new(1965, 5, 23)
-    datetime = RubyDate.new(1965, 5, 23, 22, 31, 59)
+    # datetime = DateTime.new(1965, 5, 23, 22, 31, 59)
 
-    [date, datetime].each_with_index do |d, i|
+    # [date, datetime].each_with_index do |d, i|
 
-      if i == 0
-	assert_equal('1965-05-23', d.to_s)
-      else
-	assert_equal('1965-05-23T22:31:59+00:00', d.to_s)
-      end
+    #   if i == 0
+    #     assert_equal('1965-05-23', d.to_s)
+    #   else
+    #     assert_equal('1965-05-23T22:31:59+00:00', d.to_s)
+    #   end
 
-      assert_equal('', d.inspect.gsub!(/./,''))
-      assert_equal('', d.to_s.gsub!(/./,''))
+    #   assert_equal('', d.inspect.gsub!(/./,''))
+    #   assert_equal('', d.to_s.gsub!(/./,''))
 
-      assert_equal(2438904, d.jd)
+    #   assert_equal(2438904, d.jd)
 
-      if i == 0
-	assert_equal(0, d.day_fraction)
-      else
-	assert_equal(22.to_r/24 + 31.to_r/1440 + 59.to_r/86400, d.day_fraction)
-      end
+    #   if i == 0
+    #     assert_equal(0, d.day_fraction)
+    #   else
+    #     assert_equal(22.to_r/24 + 31.to_r/1440 + 59.to_r/86400, d.day_fraction)
+    #   end
 
-      assert_equal(38903, d.mjd)
-      assert_equal(139744, d.ld)
+    #   assert_equal(38903, d.mjd)
+    #   assert_equal(139744, d.ld)
 
-      assert_equal(1965, d.year)
-      assert_equal(143, d.yday)
-      assert_equal(5, d.mon)
-      assert_equal(d.mon, d.month)
-      assert_equal(23, d.mday)
-      assert_equal(d.mday, d.day)
+    #   assert_equal(1965, d.year)
+    #   assert_equal(143, d.yday)
+    #   assert_equal(5, d.mon)
+    #   assert_equal(d.mon, d.month)
+    #   assert_equal(23, d.mday)
+    #   assert_equal(d.mday, d.day)
 
-      if i == 0
-	assert_equal(false, d.respond_to?(:hour))
-	assert_equal(false, d.respond_to?(:min))
-	assert_equal(false, d.respond_to?(:sec))
-	assert_equal(false, d.respond_to?(:sec_fraction))
-	assert_equal(false, d.respond_to?(:zone))
-	assert_equal(false, d.respond_to?(:offset))
-      else
-	assert_equal(22, d.hour)
-	assert_equal(31, d.min)
-	assert_equal(59, d.sec)
-	assert_equal(0, d.sec_fraction)
-	assert_equal('+00:00', d.zone)
-	assert_equal(0, d.offset)
-      end
+    #   if i == 0
+    #     assert_equal(false, d.respond_to?(:hour))
+    #     assert_equal(false, d.respond_to?(:min))
+    #     assert_equal(false, d.respond_to?(:sec))
+    #     assert_equal(false, d.respond_to?(:sec_fraction))
+    #     assert_equal(false, d.respond_to?(:zone))
+    #     assert_equal(false, d.respond_to?(:offset))
+    #   else
+    #     assert_equal(22, d.hour)
+    #     assert_equal(31, d.min)
+    #     assert_equal(59, d.sec)
+    #     assert_equal(0, d.sec_fraction)
+    #     assert_equal('+00:00', d.zone)
+    #     assert_equal(0, d.offset)
+    #   end
 
-      assert_equal(1965, d.cwyear)
-      assert_equal(20, d.cweek)
-      assert_equal(7, d.cwday)
+    #   assert_equal(1965, d.cwyear)
+    #   assert_equal(20, d.cweek)
+    #   assert_equal(7, d.cwday)
 
-      assert_equal(0, d.wday)
-      assert_equal(false, d.leap?)
-      assert_equal(false, d.julian?)
-      assert_equal(true, d.gregorian?)
+    #   assert_equal(0, d.wday)
+    #   assert_equal(false, d.leap?)
+    #   assert_equal(false, d.julian?)
+    #   assert_equal(true, d.gregorian?)
 
-      assert_equal(RubyDate::ITALY, d.start)
-      assert_equal(d.start, d.start)
-    end
+    #   assert_equal(RubyDate::ITALY, d.start)
+    #   assert_equal(d.start, d.start)
+    # end
+
+    assert_equal('1965-05-23', date.to_s)
+
+    assert_equal('', date.inspect.gsub!(/./,''))
+    assert_equal('', date.to_s.gsub!(/./,''))
+
+    assert_equal(2438904, date.jd)
+
+    assert_equal(0, date.day_fraction)
+
+    assert_equal(38903, date.mjd)
+    assert_equal(139744, date.ld)
+
+    assert_equal(1965, date.year)
+    assert_equal(143, date.yday)
+    assert_equal(5, date.mon)
+    assert_equal(date.mon, date.month)
+    assert_equal(23, date.mday)
+    assert_equal(date.mday, date.day)
+
+    assert_equal(false, date.respond_to?(:hour))
+    assert_equal(false, date.respond_to?(:min))
+    assert_equal(false, date.respond_to?(:sec))
+    assert_equal(false, date.respond_to?(:sec_fraction))
+    assert_equal(false, date.respond_to?(:zone))
+    assert_equal(false, date.respond_to?(:offset))
+
+    assert_equal(1965, date.cwyear)
+    assert_equal(20, date.cweek)
+    assert_equal(7, date.cwday)
+
+    assert_equal(0, date.wday)
+    assert_equal(false, date.leap?)
+    assert_equal(false, date.julian?)
+    assert_equal(true, date.gregorian?)
+
+    assert_equal(RubyDate::ITALY, date.start)
+    assert_equal(date.start, date.start)
 
     d = RubyDate.new(1965, 5, 23, 22, 31, 59) + 1.to_r/(86400*2)
     assert_equal(1.to_r/2, d.sec_fraction)
@@ -84,7 +122,7 @@ class RubyDateDateAttrTest < Test::Unit::TestCase
     d = RubyDate.new(2005, 10, 30)
     14.times do |i|
       assert((d + i).__send__(%w(sunday? monday? tuesday? wednesday?
-				 thursday? friday? saturday?)[i % 7]))
+	                         thursday? friday? saturday?)[i % 7]))
     end
   end
 
